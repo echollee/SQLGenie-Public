85991523c8c3455966aeb74ff610a982e95c97c9
echollee <lilei@dhgate.com>
2024-12-09 17:40:51 +0800

    飞书登录获认证获取信息

--- report-front-end/src/pages/FeishuSsoLogin.tsx
+++ report-front-end/src/pages/FeishuSsoLogin.tsx
@@ -1,9 +1,21 @@
 import {useEffect} from "react";
 
 import {
+    APP_LOGO,
+    APP_LOGO_DISPLAY_ON_LOGIN_PAGE, APP_VERSION,
     BACKEND_URL,
     FEISHU_APP_ID
 } from "../utils/constants";
+
+import AuthWithCognito, {
+    AppWrapper,
+    AuthTitle,
+    WrapperThemeProvider
+} from "../components/Login/AuthWithCognito/index.tsx"
+import {Amplify, Auth} from "aws-amplify";
+import {awsConfig} from "../components/Login/AuthWithFeishu/aws-config.ts";
+import AuthManager from "../components/Login/AuthWithFeishu/AuthManager.ts";
+import {Authenticator, Heading, Image, useTheme, View} from "@aws-amplify/ui-react";
 
 function loadScript(src, onload) {
     const script = document.createElement('script');
@@ -14,13 +26,25 @@ function loadScript(src, onload) {
     document.head.appendChild(script);
 }
 
+
+async function handleSignIn() {
+    try {
+        const user = await Auth.signIn("", ""); // Wait for sign-in to complete
+        console.log("Sign-in successful:", user);
+        return user
+    } catch (error) {
+        console.error("Error during sign-in:", error);
+        // return ''
+    }
+}
+
 export default function AuthWithFeishuSso() {
     useEffect(() => {
         loadScript('https://lf1-cdn-tos.bytegoofy.com/goofy/lark/op/h5-js-sdk-1.5.33.js', () => {
-            console.log('H5 SDK loaded');
             if (!window.h5sdk) {
                 alert('H5 SDK failed to initialize');
             } else {
+                console.log('H5 SDK loaded');
                 window.h5sdk.ready(function () {
                     // console.log('FEISHU_APP_ID',FEISHU_APP_ID);
                     tt.requestAccess({
@@ -42,6 +66,37 @@ export default function AuthWithFeishuSso() {
                                 }
                             const username = email.substring(0, email.indexOf("@"))
                             console.log('username:', username)
+
+
+                            console.log("Cognito configured");
+                            try {
+                                Amplify.configure(awsConfig);
+                                console.log('loginCount:', AuthManager.loginCount);
+                                if (AuthManager.loginCount > 0) {
+                                    console.log('User is already logged in.');
+                                } else {
+                                    console.log('User logging...');
+                                    AuthManager.incrementLoginCount();
+                                    handleSignIn().then((user) => {
+                                        // console.log('User info:', user);
+                                        if (user) {
+                                            (async () => {
+                                                console.log('user.username:', user.username)
+                                                while (!user.username) {
+                                                    console.log('Waiting for username...');
+                                                    await new Promise((resolve) => setTimeout(resolve, 3000));
+                                                }
+                                                AuthManager.incrementLoginCount();
+                                            })();
+                                        } else {
+                                            console.log('User sign in failed or user session is missing.');
+                                            alert('用户登录失败，请提交飞书流程申请权限，或联系管理员！');
+                                        }
+                                    });
+                                }
+                            } catch (e) {
+                                console.error(e);
+                            }
                         },
                         fail(res) {
                             console.log(`requestAccess fail: ${JSON.stringify(res)}`);
@@ -56,4 +111,52 @@ export default function AuthWithFeishuSso() {
             <h2>我是Tab2</h2>
         </main>
     );
+    /*return (
+        <WrapperThemeProvider>
+            <Authenticator
+                signUpAttributes={["email"]}
+                components={{
+                    Header() {
+                        const {tokens} = useTheme();
+                        return !APP_LOGO_DISPLAY_ON_LOGIN_PAGE ? (
+                            <AuthTitle/>
+                        ) : (
+                            <View
+                                textAlign="center"
+                                margin={`${tokens.space.xxl} auto`}
+                                position="relative"
+                            >
+                                {APP_LOGO && (
+                                    <Image alt="App logo" src={APP_LOGO} width={200}/>
+                                )}
+                                <View padding={tokens.space.small}>
+                                    <Heading fontWeight="400" level={3}>
+                                        Generative Business Intelligence
+                                    </Heading>
+                                    {APP_VERSION && (
+                                        <Heading fontWeight="200">{APP_VERSION}</Heading>
+                                    )}
+                                </View>
+
+                                <Heading fontWeight="200">
+                                    Guidance on Amazon Web Services
+                                </Heading>
+                                {!APP_LOGO && (
+                                    <Image
+                                        width="50px"
+                                        alt="Amazon Web Services Logo"
+                                        src="/smile-logo.png"
+                                    />
+                                )}
+                            </View>
+                        );
+                    },
+                }}
+            >
+                {({signOut, user}) => (
+                    <AppWrapper signOut={signOut} user={user as any}/>
+                )}
+            </Authenticator>
+        </WrapperThemeProvider>
+    );*/
 }
